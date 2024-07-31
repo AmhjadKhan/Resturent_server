b3a0aecc9aa86549dbac9c09fd6aa41b0d6f6171
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-01 02:50:50 +0400

    this is verifytoken added commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -34,42 +35,40 @@ async function run() {
     const cartCollection = client.db("resturentDb").collection("carts");
     const userCollection = client.db("resturentDb").collection("users");
 
-     
-    //  menu collection 
-    app.get('/menu', async(req,res) =>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    })
-    // review collection 
-    app.get('/reviews', async(req,res)=>{
-       const result = await reviewCollection.find().toArray();
-       res.send(result)
-    })
-  
-    // cart collection 
-     
-    // post cart method 
-    app.post('/carts', async(req, res) =>{
-      const cartItem = req.body;
-      const result = await cartCollection.insertOne(cartItem);
-      res.send(result);
+    // jwt access token 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     })
 
-    //get cart method 
-    app.get('/carts', async (req, res) => {
-      const email = req.query.email;
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+     // use verify admin after verifyToken
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
       const query = { email: email };
-      const result = await cartCollection.find(query).toArray();
-      res.send(result);
-    });
-
-    // cart delete method 
-    app.delete('/carts/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await cartCollection.deleteOne(query);
-      res.send(result);
-    })
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
 
 
     // user releted api 
@@ -88,6 +87,7 @@ async function run() {
 
     // user get 
     app.get('/users',async (req, res) => {
+      console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -113,6 +113,44 @@ async function run() {
       res.send(result);
     })
 
+     
+    //  menu collection 
+    app.get('/menu', async(req,res) =>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+    // review collection 
+    app.get('/reviews', async(req,res)=>{
+       const result = await reviewCollection.find().toArray();
+       res.send(result)
+    })
+  
+    // cart collection   
+    // post cart method 
+    app.post('/carts', async(req, res) =>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    //get cart method 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // cart delete method 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
