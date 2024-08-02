08626e1e2de4496ad6f77b2d14ebe73c00e99026
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-02 19:46:23 +0400

    this is payment intern commit post

--- index.js
+++ index.js
@@ -214,6 +214,22 @@ async function run() {
       res.send(result);
     })
 
+// payment method 
+app.post('/create-payment-intent', async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, 'amount inside the intent')
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+});
 
     
     // Send a ping to confirm a successful connection
